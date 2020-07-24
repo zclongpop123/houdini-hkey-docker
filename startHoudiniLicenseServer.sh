21c9f09735e3b14b1f167ed5368142a7753ac625
Changlong.Zang <zclongpop123@163.com>
2020-07-24 11:28:40 +0800

    Update startHoudiniLicenseServer.sh

--- startHoudiniLicenseServer.sh
+++ startHoudiniLicenseServer.sh
@@ -7,4 +7,4 @@
 #/usr/lib/sesi/sesinetd_safe --sesi=/usr/lib/sesi --sesinetd=/usr/lib/sesi/sesinetd --log-file=/dev/stdout --pid-file=/var/run/sesinetd.pid -V 3 -z 0 -y 0 -m *.*.*.* -M *.*.*.* -p 1717 -W 1 -l /dev/stdout -u /dev/stdout
 
 # Finally, the actual binary and it's flags
-/usr/lib/sesi/sesinetd -l /dev/stdout -D -V 3 -z 0 -y 0 -m *.*.*.* -M *.*.*.* -p 1715 -W 1 -l /dev/stdout -u /dev/stdout -R /var/run/sesinetd.pid
+/usr/lib/sesi/sesinetd -D -V 3 -z 0 -y 0 -m *.*.*.* -M *.*.*.* -p 1715 -W 1 -l /dev/stdout -u /dev/stdout -R /var/run/sesinetd.pid
